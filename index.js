5d57be5cd8f6a63c946b1674654c18bafb25e501
erikgardea <erik.gardea@zendesk.com>
2022-05-24 18:43:40 -0500

    Added env vars

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const SunshineConversationsApi = require('sunshine-conversations-client');
 const Axios = require('axios');
 const { default: axios } = require('axios');
 const express = require('express');
 const cors = require('cors');
-
 const defaultClient = SunshineConversationsApi.ApiClient.instance;
 const basicAuth = defaultClient.authentications['basicAuth'];
-basicAuth.username = 'app_626961d7213c3200f315d071	'; // e.g. 'app_5deaa3531c7f940010cc4ba4'
-basicAuth.password = 'vbDY1lAAQKuK27Y2fBbD1y8nP1g77UPLK1FH1jOsLoUe2ar953awwZGYroFJQQim01KXFOJ7kfIvVArr-6Uwow'; // e.g. 'tHyBAxPQIX_8CQNEefdc8L8B'
+basicAuth.username = process.env.USERNAME; // e.g. 'app_5deaa3531c7f940010cc4ba4'
+basicAuth.password = process.env.PASSWORD; // e.g. 'tHyBAxPQIX_8CQNEefdc8L8B'
 
 const apiInstance = new SunshineConversationsApi.MessagesApi();
 const port = process.env.PORT || 8000;
@@ -20,7 +20,7 @@ APP.get('/templates', function(req, res) {
         method: 'GET',
         headers: {
             'Content-type': 'application/json',
-            "Authorization": "Basic YXBwXzYyNjk2MWQ3MjEzYzMyMDBmMzE1ZDA3MTp2YkRZMWxBQVFLdUsyN1kyZkJiRDF5OG5QMWc3N1VQTEsxRkgxak9zTG9VZTJhcjk1M2F3d1pHWXJvRkpRUWltMDFLWEZPSjdrZkl2VkFyci02VXdvdw=="
+            "Authorization": `Basic ${process.env.BEARER_TOKEN}`
         },
         redirect: 'follow'
     })
