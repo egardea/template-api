cd041e35e6370282af00e5745319de885d65eb46
erikgardea <erik.gardea@zendesk.com>
2022-06-17 17:29:18 -0500

    Added delete route

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 require('dotenv').config();
-const { default: axios } = require('axios');
 const SmoochCore = require('smooch-core');
 const express = require('express');
 const cors = require('cors');
 
 const port = process.env.PORT || 8000;
 const APP = express()
-let appId = null;
-let smooch;
 
 APP.use(cors());
 APP.use(express.json());
 
-APP.post('/', function(req, res) {
-  appId = req.body.appId;
-  smooch = new SmoochCore({keyId: req.body.key, secret: req.body.secret, scope: 'app'});
-});
-
 APP.get('/templates', function(req, res) {
+  let smooch = new SmoochCore({keyId: req.query.keyId, secret: req.query.secret, scope: 'app'});
+
   smooch.templates.list({
-    appId: appId,
+    appId: req.query.appId,
     props: {
       limit: 100
     }
@@ -29,10 +23,23 @@ APP.get('/templates', function(req, res) {
 });
 
 APP.post('/create', function(req, res) {
-    smooch.templates.create({
-      appId: appId,
+  let smooch = new SmoochCore({keyId: req.query.keyId, secret: req.query.secret, scope: 'app'});
+
+  smooch.templates.create({
+      appId: req.query.appId,
       props: req.body
-    })
+  })
+    .then(result => res.send(result))
+    .catch(error => res.send(error));
+});
+
+APP.delete('/delete', function(req, res) {
+  let smooch = new SmoochCore({keyId: req.query.keyId, secret: req.query.secret, scope: 'app'});
+
+  smooch.templates.delete({
+      appId: req.query.appId,
+      templateId: req.query.templateId
+  })
     .then(result => res.send(result))
     .catch(error => res.send(error));
 });
